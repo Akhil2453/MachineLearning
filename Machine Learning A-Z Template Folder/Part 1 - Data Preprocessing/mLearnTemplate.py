3c59acec8aeaf55f17b1995fdf8218575dc0afa5
Akhil2453 <akhil.menon94@gmail.com>
2018-05-23 11:53:56 +0530

    set working directory as Part1-...

---
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/mLearnTemplate.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 23 11:44:45 2018
+
+@author: Akhil
+"""
+
+#importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+#importing the dataset
