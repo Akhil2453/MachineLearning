ff656e2154800ee4a4ea7677e5f3fcba20ce504d
Akhil2453 <akhil.menon94@gmail.com>
2018-05-23 14:05:12 +0530

    include library and create object to preprocess data, to fine the missing element

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/mLearnTemplate.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/mLearnTemplate.py
@@ -17,3 +17,8 @@ dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values   #storing feature data(country,age,salary), in x
                                   #iloc[:, :-1] selects the first three columns of the dataset
 y = dataset.iloc[:, 3].values     # storing dependent vector in y                               
+
+#taking care of missing data
+from sklearn.preprocessing import Imputer 
+                    #sklearn includes methods to preprocess datasets.
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
