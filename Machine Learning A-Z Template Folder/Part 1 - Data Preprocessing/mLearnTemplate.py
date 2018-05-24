2ab0d6cb1539d72bdcd06fa22066b3362c71d3c2
Akhil2453 <akhil.menon94@gmail.com>
2018-05-24 10:32:11 +0530

    scaled festures of age and salary to standardise and normalise them

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/mLearnTemplate.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/mLearnTemplate.py
@@ -39,5 +39,12 @@ y = labelencoder_x.fit_transform(y)
 
 #Splitting the dataset into training and test set
 from sklearn.cross_validation import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+x_train,  x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
 
