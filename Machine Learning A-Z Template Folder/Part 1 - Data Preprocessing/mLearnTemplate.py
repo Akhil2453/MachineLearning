98101c44431fffbfa0ed3e786f7095ae9a80c741
Akhil2453 <akhil.menon94@gmail.com>
2018-05-24 10:56:58 +0530

    Data preprocessing template - final

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/mLearnTemplate.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/mLearnTemplate.py
@@ -19,15 +19,16 @@ x = dataset.iloc[:, :-1].values   #storing feature data(country,age,salary), in 
 y = dataset.iloc[:, 3].values     # storing dependent vector in y                               
 
 #taking care of missing data
-from sklearn.preprocessing import Imputer 
+"""from sklearn.preprocessing import Imputer 
                     #sklearn includes methods to preprocess datasets.
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 
 imputer = imputer.fit(x[:, 1:3])
-x[:, 1:3] = imputer.transform(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])"""
+#not necessary in all mLearning example
 
 # Encoding categorical Data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+"""from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_x = LabelEncoder()
 x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
 #Use onehot encoder, as there's no comparison between countries, and we don't want the algorithm to compare them
@@ -35,7 +36,8 @@ onehotencoder = OneHotEncoder(categorical_features = [0])
 x = onehotencoder.fit_transform(x).toarray()
 #use label encoder, as we have tocompare the purchases and it's dependent vector
 labelencoder_y = LabelEncoder()
-y = labelencoder_x.fit_transform(y)
+y = labelencoder_x.fit_transform(y)"""
+#not necessary in all mLearning examples
 
 #Splitting the dataset into training and test set
 from sklearn.cross_validation import train_test_split
@@ -43,8 +45,9 @@ x_train,  x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, rand
 
 
 #Feature Scaling
-from sklearn.preprocessing import StandardScaler
+"""from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)
+x_test = sc_x.transform(x_test)"""
+#not necessary in all example
 
